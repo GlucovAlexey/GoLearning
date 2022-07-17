42e507dd37c9dce76fa24ee5b1e67f77cabbc0ca
Alexey <gluchov44@gmail.com>
2022-07-17 20:54:44 +0300

    Task

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	var num string
+	fmt.Scan(&num)
+	var a = num[0]
+	var b = num[1]
+	var c = num[2]
+
+	switch {
+	case a != b && b != c && c != a:
+		fmt.Print("YES")
+	default:
+		fmt.Print("NO")
+	}
 }
