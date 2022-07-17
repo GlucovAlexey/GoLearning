eb112fe74415137b58bb1fe3c7873216ce513482
Alexey <gluchov44@gmail.com>
2022-07-17 22:31:00 +0300

    Task2

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var num int
-	fmt.Scan(&num)
-	first := num / 100000
-	seconf := num / 10000 % 10
-	third := num / 1000 % 10
-	forth := num / 100 % 10
-	fifth := num / 10 % 10
-	sixth := num % 10
-	switch {
-	case (first + seconf + third) == (forth + fifth + sixth):
-		fmt.Print("YES")
-	default:
-		fmt.Print("NO")
+	var num, res, greate int
+	num = 1
+	for num != 0 {
+		fmt.Scan(&num)
+		if greate < num {
+			greate = num
+			res = 0
+		}
+		if greate == num {
+			res++
+		}
 	}
+	fmt.Print(res)
 }
