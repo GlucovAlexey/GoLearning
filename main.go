80b10597abf2250d2c754dc0c8ec8bd70a3668a0
Alexey <gluchov44@gmail.com>
2022-08-01 21:34:18 +0300

    commit

--- main.go
+++ main.go
@@ -3,24 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	workArray := [10]uint8{}
-	var a uint8
-	for i := 0; i < 10; i++ {
-		fmt.Scan(&a)
-		workArray[i] = a
-	}
-	array := [6]uint8{}
-	var b uint8
-	for i := 0; i < 6; i++ {
-		fmt.Scan(&b)
-		array[i] = b
-	}
-	workArrayCopy := workArray
-	for i := 0; i < 5; i += 2 {
-		workArray[array[i]] = workArrayCopy[array[i+1]]
-		workArray[array[i+1]] = workArrayCopy[array[i]]
-	}
-	for i := 0; i < 10; i++ {
-		fmt.Print(workArray[i], " ")
-	}
+
+  var count int
+  fmt.Scan(&count)
+  workArray := []int{}
+  var a int
+	
+  for i := 0; i <= count; i++ {
+	fmt.Scan(&a)
+    workArray = append(workArray, a)
+  }
+  for i := 0; i < count; i++{
+    if workArray[i] % 2 == 0{
+      fmt.Printf(" %d ", workArray[i])
+    }
+  }
 }
